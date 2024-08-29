4664d08e95929381b3891f8a5c7c48806114c468
Marco Ulloa Toro <m.ullloatoro@gmail.com>
2024-08-29 12:51:33 -0500

    push 28auh_2

--- project_sql/6. optimize_query_5.sql
+++ project_sql/6. optimize_query_5.sql
@@ -7,7 +7,7 @@ INNER JOIN skills_job_dim ON skills_job_dim.job_id = job_postings_fact.job_id
 INNER JOIN skills_dim ON skills_dim.skill_id = skills_job_dim.skill_id 
 WHERE
    job_postings_fact.job_work_from_home = TRUE
-   AND job_postings_fact.job_title = 'Data Analyst'
+   AND job_postings_fact.job_title_short = 'Data Analyst'
    AND job_postings_fact.salary_year_avg IS NOT NULL
 GROUP BY
    skills_dim.skill_id
@@ -16,5 +16,7 @@ HAVING
 ORDER BY
    avg_salary DESC,
    num_jobs DESC
+LIMIT 15
+
 
 
